08d813dca7971f08d2e0dd9859b4719f5284efe0
Raihan Ramadistra <ramadistra@gmail.com>
2019-10-18 20:19:08 +0700

    add foldexpression

--- excerise/latihan-uts/LetExpression.hs
+++ excerise/latihan-uts/LetExpression.hs
@@ -24,6 +24,46 @@ evaluate (e1 :* e2) = evaluate e1 * evaluate e2
 evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
 evaluate (Let v e0 e1) = evaluate (subst v e0 e1)
 
+data ExprCombinator a = ExprCombinator { constant :: Float -> a
+                                       , plus :: a -> a -> a
+                                       , minus :: a -> a -> a
+                                       , mul ::  a -> a -> a
+                                       , divide ::  a -> a -> a
+                                       , var :: String -> a
+                                       , bind :: (String, Expr, Expr) -> a
+                                       }
+
+foldExpr cmb (C x) = constant cmb x
+foldExpr cmb (e1 :+ e2) = (plus cmb) (foldExpr cmb e1) (foldExpr cmb e2)
+foldExpr cmb (e1 :- e2) = (minus cmb) (foldExpr cmb e1) (foldExpr cmb e2)
+foldExpr cmb (e1 :* e2) = (mul cmb) (foldExpr cmb e1) (foldExpr cmb e2)
+foldExpr cmb (e1 :/ e2) = (divide cmb) (foldExpr cmb e1) (foldExpr cmb e2)
+foldExpr cmb (V v) = var cmb v
+foldExpr cmb (Let v e0 e1) = bind cmb (v, e0, e1)
+
+evalWithFold :: Expr -> Float
+evalWithFold = foldExpr $ 
+  ExprCombinator { constant = id
+                 , plus = (+)
+                 , minus = (-)
+                 , mul = (*)
+                 , divide = (/)
+                 , var = undefined
+                 , bind = evalWithFold . substitueWithFold
+                 }
+
+substitueWithFold :: (String, Expr, Expr) -> Expr
+substitueWithFold (v0, e0, e1) = foldExpr cmb e1 
+  where 
+    cmb = ExprCombinator { constant = C
+                         , plus = (:+)
+                         , minus = (:-)
+                         , mul = (:*)
+                         , divide = (:/)
+                         , var = sub
+                         , bind = (Let v0 e0) . substitueWithFold
+                         }
+    sub v1 = if (v0 == v1) then e0 else (V v1)
 
 x = Let "x" (C 2) $
       Let "y" (C 3) $ 
